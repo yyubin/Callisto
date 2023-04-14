458c7c266647e3a0b7cb732595ea38f43e657646
yyubin <hazing120@gmail.com>
2023-04-15 02:39:57 +0900

    feat: Set chat server

---
+++ callisto-chat-go-server/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"chat/chat"
+	"flag"
+)
+
+var (
+	port = flag.String("p", ":8080", "set port")
+)
+
+func init() {
+	flag.Parse()
+}
+
+func main() {
+	chat.Start(*port)
+}

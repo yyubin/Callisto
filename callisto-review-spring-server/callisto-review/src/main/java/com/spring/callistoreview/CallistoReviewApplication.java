efad3ceea67ced3d5b156dea680b7c9b29bf5ce4
yyubin <hazing120@gmail.com>
2023-04-16 00:50:18 +0900

    feat: Review server db connection

---
+++ callisto-review-spring-server/callisto-review/src/main/java/com/spring/callistoreview/CallistoReviewApplication.java
@@ -0,0 +1,13 @@
+package com.spring.callistoreview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CallistoReviewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CallistoReviewApplication.class, args);
+    }
+
+}
---
+++ callisto-review-spring-server/callisto-review/src/test/java/com/spring/callistoreview/CallistoReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.callistoreview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CallistoReviewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

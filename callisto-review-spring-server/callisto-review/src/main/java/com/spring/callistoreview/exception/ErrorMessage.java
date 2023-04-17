850ceba44d6f504f4f48c4e48c94158b8cb21b7a
yyubin <hazing120@gmail.com>
2023-04-18 03:25:58 +0900

    refactor: Update errorMessage

--- callisto-review-spring-server/callisto-review/src/main/java/com/spring/callistoreview/exception/ErrorMessage.java
+++ callisto-review-spring-server/callisto-review/src/main/java/com/spring/callistoreview/exception/ErrorMessage.java
@@ -2,7 +2,7 @@ package com.spring.callistoreview.exception;
 
 public enum ErrorMessage {
     NOT_FOUND_PROFILE("유저를 찾을 수 없습니다."),
-    NOT_MATCH_USER_COMPANY("해당하는 회사의 유저를 찾을 수 없습니다."),
+    NOT_MATCH_USER_COMPANY("재직중인 회사만 리뷰할 수 있습니다."),
     ;
 
     private final String message;
